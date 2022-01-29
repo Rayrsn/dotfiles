76b3215d004fe3b24e5ddd79822c5c699ead6708
Rayr <25773909+Rayrsn@users.noreply.github.com>
2022-01-29 20:27:20 +0330

    Add files via upload

---
+++ .config/fish/fishsetup.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# Only Install in Linux
+if [ "$(uname)" = "Linux" ]; then
+    # check distro
+    if [ "$(cat /etc/os-release | grep ID_LIKE)" = "ID_LIKE=ubuntu" ]; then
+    echo "Current distro is Ubuntu/Ubuntu Based"
+    echo "Installing Fish..."
+    sudo apt install fish -y
+    
+    fi
+
+    if [ "$(cat /etc/os-release | grep ID_LIKE)" = "ID_LIKE=arch" ]; then
+    echo "Current distro is Arch/Arch Based"
+    echo "Installing Fish..."
+    sudo pacman -S fish --noconfirm
+    fi
+# Set fish as default shell
+chsh -s /bin/fish
+
+# function for installing fisher
+function fisher_install() {
+    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+}
+
+# function for installing oh my fish
+function omf_install() {
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+}
+
+# function for installing fish configs
+function fishconfig_install() {
+    cp . ~/.config/fish/
+}
+
+# if argument noconfirm or nc is passed, run without asking else ask
+if [ "$1" = "--no-confirm" ] || [ "$1" = "-nc" ]; then
+    fisher_install
+    omf_install
+    fishconfig_install
+else
+
+    # Ask if user wants to install oh my fish
+    while true; do
+        read -p "Do you wish to install oh my fish? [Y/n] " yn
+        case $yn in
+            [Yy]* ) omf_install; break;;
+            [Nn]* ) exit;;
+            * ) omf_install;;
+        esac
+    done
+
+    # Ask if user wants to install fihser
+    while true; do
+        read -p "Do you wish to install fisher? [Y/n] " yn
+        case $yn in
+            [Yy]* ) fisher_install; break;;
+            [Nn]* ) exit;;
+            * ) fisher_install;;
+        esac
+    done
+
+
+    # Ask if user wants to install the fish configs
+    while true; do
+        read -p "Do you wish to install fish configs? [Y/n] " yn
+        case $yn in
+            [Yy]* ) fishconfig_install; break;;
+            [Nn]* ) exit;;
+            * ) fishconfig_install;;
+        esac
+    done
+
+fi
+fi
